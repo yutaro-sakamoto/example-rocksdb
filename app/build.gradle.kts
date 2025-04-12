5bf8aba30ac202c5a7b340d24680afa3be0e420c
Yutaro Sakamoto <mail@yutaro-sakamoto.com>
2025-04-12 10:09:04 +0900

    build: add `rocksdb` dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,6 +23,7 @@ dependencies {
 
     // This dependency is used by the application.
     implementation(libs.guava)
+    implementation("org.rocksdb:rocksdbjni:9.10.0")
 }
 
 // Apply a specific Java toolchain to ease working on different environments.
